2f022b25ea52264af5d080ffab0e94b51a4a8a0e
Greg W <gcwestover@gmail.com>
2021-02-17 09:34:33 -0500

    Added color picker and erase on right-click

--- sketch.js
+++ sketch.js
@@ -1,26 +1,50 @@
+// let colorPicker;
+// function setup() {
+//   createCanvas(100, 100);
+//   colorPicker = createColorPicker('#ed225d');
+//   colorPicker.position(0, height + 5);
+// }
+
+// function draw() {
+//   background(colorPicker.color());
+// }
+
 var x1;
 var x2;
 var y1;
 var y2;
+let colorPicker;
 
 function setup() {
-  var canvas = createCanvas(1000, 700);
+  const canvas = createCanvas(1000, 700);
   background(51);
+  colorPicker = createColorPicker('#ffffff');
+  colorPicker.position(width/2 - 50, height + 5);
   canvas.parent("sketch");
 }
 
 function draw() {
-  stroke(255);
-  strokeWeight(1);
   x1 = mouseX;
   y1 = mouseY;
   if (mouseIsPressed == true) {
-    line(x1, y1, x2, y2);
+    if (mouseButton == LEFT) {
+      stroke(colorPicker.color());
+      strokeWeight(2);
+      line(x1, y1, x2, y2);
+    }
+    else if (mouseButton == RIGHT) {
+      console.log("Right click!");
+      stroke(51);
+      fill(51);
+      ellipse(mouseX,mouseY,20);
+    }
   }
   x2 = mouseX;
   y2 = mouseY;
 
   if (keyIsPressed == true) {
-    background(51);
+    if (keyCode == 32) {
+      background(51);
+    }
   }
 }
